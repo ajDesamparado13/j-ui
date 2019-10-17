3a7d7ec5c443501d7166fa1ca428cdfd4eba32bb
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2019-10-17 18:56:41 +0800

    Update aliases

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 
 // vue.config.js
 module.exports = {
   chainWebpack: config => {
     config.module.rules.delete('eslint')
+    config.resolve.alias.set('@docs', resolve('docs'))
   },
   css: {
     loaderOptions: {
