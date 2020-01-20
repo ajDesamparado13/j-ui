9fbc5a1663cd50ef985c0337264372378fb2b5d2
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-01-20 11:10:18 +0800

    Update Pagination structure and fix for pager

--- src/components/pagination/Pagination.vue
+++ src/components/pagination/Pagination.vue
@@ -1,163 +1,183 @@
 <template>
-    <div class="ui-pagination">
-        <div class="pagination">
-            <div class="ui-pagination-item prev-page">
-                <ui-button
-                    type="secondary"
-                    @click="prevPage"
-                    :disabled="value == 1"
-                    :size="size"
-                    >
-                    <ui-icon icon="chevron-left"/>
-                </ui-button>
-            </div>
-            <div class="ui-pagination-item start-page" v-if="startPage != 1">
-                <ui-button type="secondary" color="primary" :size="size" @click="load(1)">1</ui-button>
-                <span class="is-unselectable"> ...  </span>
-            </div>
-            <div class="ui-pagination-item" v-for="page in total" v-if="page >= startPage && page <= endPage">
-                <ui-button class="select-page" v-if="page != value"
-                    color="primary" type="secondary" :size="size" @click="load(page)">{{page}}</ui-button>
-                <ui-button class="current-page" v-if="page == value" type="primary" color="primary" :size="size" :disabled="true">{{page}}</ui-button>
-            </div>
-            <div class="ui-pagination-item end-page"  v-if="endPage < total">
-                <span class="is-unselectable"> ...  </span>
-                <ui-button type="secondary" color="primary" :size="size" @click="load(total)">{{total}}</ui-button>
-            </div>
-            <div class="ui-pagination-item next-page">
-                <ui-button
-                    type="secondary"
-                    @click="nextPage"
-                    :disabled="disableNextPage || value==total"
-                    :size="size"
-                    ><ui-icon icon="chevron-right"/></ui-button>
-            </div>
-        </div>
+  <div class="ui-pagination">
+    <div class="pagination-container">
+        <ui-button class="pagination-item prev-page" type="secondary" @click="prevPage" :disabled="value == 1" >
+          <ui-icon icon="chevron-left" />
+        </ui-button>
+        <template v-for="( page,index ) in pages">
+          <component
+          class="pagination-item"
+          :class="{
+           'select-page':page != value,
+           'current-page':page == value,
+           'is-unselectable':getElement(page) == 'span'
+          }"
+          :type="page==value?'primary':'secondary'"
+          color="primary"
+          :disabled="page==value"
+          @click="load(page)"
+           :id="`pagination-item-${page}`"
+           :is="getElement(page)">
+           {{page}}
+          </component>
+        </template>
+        <ui-button class="pagination-item next-page"
+          type="secondary"
+          @click="nextPage"
+          :disabled="disableNextPage || value==total"
+        >
+          <ui-icon icon="chevron-right" />
+        </ui-button>
     </div>
+  </div>
 </template>
 
+
 <script>
-export default{
-    name:'ui-pagination',
-    props:{
-        size:{
-            type:String,
-            default:'small',
-        },
-        maxPager:{
-            type:Number,
-            default:5,
-        },
-        value:{
-            type:[String,Number],
-            required:true,
-        },
-        total:{
-            type:Number,
-            default:1,
-        },
-        disableNextPage:{
-            type:Boolean,
-            default:false,
-        }
+export default {
+  name: "ui-pagination",
+  props: {
+    /*
+    * Minimum number of pages that results to ellipsis
+    */
+    minimumHiddenPages:{
+      type:Number,
+      default:2,
     },
-    computed:{
-        endPage(){
-            var endPage = this.startPage + this.maxPager-1;
-
-            if(endPage >= this.total){
-                endPage = this.total;
-            }
-
-            return endPage;
-        },
-        startPage(){
-            var total = this.total;
-            var maxPager = this.maxPager;
-            var currentPage = Number(this.value);
-
-            var quotient = this.maxPager/2;
-            var lowestDenom = Math.floor(quotient);
-            var highestDenom = Math.ceil(quotient);
-
-            if(total <= maxPager || currentPage <= highestDenom){
-                return 1;
-            }
-
-            if(total - currentPage >= lowestDenom ){
-                return currentPage-lowestDenom;
-            }
-
-            if(total == currentPage){
-                return  currentPage - ( maxPager-1 );
-            }
-
-            var startPage = currentPage-highestDenom;
-
-            if(startPage < 1){
-                startPage = 1;
-            }
-
-            return startPage;
-        },
+    /*
+    * Maximum number of page selection items
+    */
+    maxPager: {
+      type: Number,
+      default: 5
     },
-    methods:{
-        nextPage() {
-            var page = Number(this.value);
-            if(this.total > page) {
-                this.load(page+1)
-            }
-        },
-        prevPage() {
-            var page = Number(this.value);
-            if(page != 1) {
-                this.load(page-1)
-            }
-        },
-        load(page){
-            this.$emit('input',page);
-            this.$emit('change',page);
-        },
+    /*
+    * Current Page Selected
+    */
+    value: {
+      type: [String, Number],
+      required: true
     },
-}
+    /*
+    * Total number of Pages
+    */
+    total: {
+      type: Number,
+      default: 1
+    },
+    /*
+    * Flag for disabling the next page
+    */
+    disableNextPage: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    pages(){
+      var ellipsis = '...';
+      var pages = this.range(this.startPage,this.endPage);
+      var count = pages.length -1;
+      var lowest = pages[0];
+      var highest = pages[count];
+      var minimum = this.minimumHiddenPages;
+      var total = this.total;
+
+
+      var sum = 1+minimum;
+      if(sum <= lowest){
+         pages.unshift( sum == lowest ? this.range(1,minimum) : ellipsis)
+         pages = pages.flat();
+      }
+
+
+      var difference = total - minimum;
+      if(difference >= highest){
+        pages = pages.concat(difference == highest ? this.range(total-minimum+1,total) : ellipsis);
+      }
+
+      if(pages.indexOf(1) == -1){
+        pages.unshift(1);
+      }
+
+      if(pages.indexOf(total) == -1){
+        pages.push(total);
+      }
+
+      return pages;
+    },
+    endPage() {
+      var endPage = this.startPage + this.maxPager - 1;
+      return endPage >= this.total ? this.total : endPage;
+    },
+    startPage() {
+      var { total, maxPager} = this;
+      var currentPage = Number(this.value);
+      var quotient = maxPager / 2;
+      var highestDenom = Math.ceil(quotient);
+      var lowestDenom = Math.floor(quotient);
+
+      if (total <= maxPager || currentPage <= highestDenom) {
+        return 1;
+      }
+
+      var startPage = 1;
+
+      if (total - currentPage >= lowestDenom) {
+        startPage = currentPage - highestDenom;
+      } else if (total == currentPage) {
+        startPage = currentPage - (maxPager - 1);
+      }else{
+        startPage = currentPage - highestDenom;
+      }
+
+      return startPage <= 1 ? 1 : startPage;
+    }
+  },
+  methods: {
+    getElement(page){
+      return !isNaN(Number(page)) > 0 ? 'ui-button' : 'span'
+    },
+    range(start,end){
+      return Array(end - start + 1).fill().map((_,index) =>  start+ index);
+    },
+    nextPage() {
+      return this.load(this.value,1)
+    },
+    prevPage() {
+      return this.load(this.value,-1)
+    },
+    load(value,add=0) {
+      var page = value+add;
+      if(!isNaN(page) && page != this.value && page >= 1 && page <= this.total){
+        this.$emit("input", page);
+        this.$emit("change", page);
+      }
+      return page;
+    }
+  }
+};
 </script>
-
 <style lang="scss">
 .ui-pagination {
-    display:flex;
-    width:100%;
-    justify-content:center;
-
-    .pagination{
-        .prev-page,.next-page{
-            @media only screen and (max-width:$bp-hands-wide){
-                max-width:rem-calc(30px);
-            }
-        }
-        display:flex;
-        @media only screen and (max-width:$bp-hands-wide){
-            flex-wrap:nowrap;
-        }
-        align-items:center;
-        .ui-button{
-            min-width:1rem;
-            border:none;
-        }
-        .button{
-            opacity:initial
-        }
-        .end-page,.start-page{
-            display:flex;
-            align-items:center;
-            span:first-child{ margin-right:rem-calc(10px); }
-            span:last-child{ margin-left:rem-calc(10px); }
-        }
-        .ui-pagination-item{
-            &:not(:last-child){
-                margin-right:rem-calc(3px);
-            }
-        }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .pagination-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    .pagination-item {
+      opacity: initial;
+      margin: 0.5rem;
+      min-width: 1rem;
+      &.prev-page,
+      &.next-page {
+        max-width: 30px;
+      }
     }
+  }
 }
-
 </style>
