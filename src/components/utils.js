ab1b0041226585fbe562779f779a064fcf498e11
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-06-17 19:40:17 +0800

    Add utility for prop setting

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,17 @@
 import utils from '../utils'
 
+export const propSetter = (componentProps,paramProps){
+  let propsData = {}
+    for(let key in componentProps){
+        let prop = props[key];
+        if(typeof paramsProps[key] == 'undefined' ){
+          propsData[key] = prop.default || prop;
+        }else{
+          propsData[key] = paramProps[key];
+        }
+    }
+    return propsData;
+}
+
+
 export default utils
