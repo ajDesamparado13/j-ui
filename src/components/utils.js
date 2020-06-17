1d310f02e0e92d7d882c29e823e3e29399166061
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-06-17 19:44:01 +0800

    Fix error

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,17 +1,16 @@
 import utils from '../utils'
 
-export const propSetter = (componentProps,paramProps){
+export const propSetter = (componentProps, paramProps) => {
   let propsData = {}
-    for(let key in componentProps){
-        let prop = props[key];
-        if(typeof paramsProps[key] == 'undefined' ){
-          propsData[key] = prop.default || prop;
-        }else{
-          propsData[key] = paramProps[key];
-        }
+  for (let key in componentProps) {
+    let prop = props[key]
+    if (typeof paramsProps[key] === 'undefined') {
+      propsData[key] = prop.default || prop
+    } else {
+      propsData[key] = paramProps[key]
     }
-    return propsData;
+  }
+  return propsData
 }
-
 
 export default utils
