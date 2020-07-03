04acf2043e2a1d136a3256209fdaa10a5a56ad01
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-07-03 09:32:45 +0800

    Update Simple Table

--- src/components/table/SimpleTable.vue
+++ src/components/table/SimpleTable.vue
@@ -1,22 +1,43 @@
 <template>
   <table class="ui-table table" :class="classes">
     <thead v-if="hasColumns">
-      <component :is="getHeaderComponent()" v-for="(column,colNum) in columns" :key="colNum"> {{ column.label }} </component>
-
+      <component :class="[ `header-cell-${column.name}` ]" :is="getHeaderComponent(column)" v-for="(column,colNum) in columns" :key="colNum"> {{ column.label }} </component>
     </thead>
     <tbody >
         <template v-if="hasData && hasColumns">
-          <component :is="getRowComponent()" v-for="(datum,rowNum) in items" :key="rowNum">
+          <component :class="`row-${datum.id}`" :is="getRowComponent()" v-for="(datum,rowNum) in items" :key="rowNum">
 
-            <component :is="getDataComponent()"  v-for="(column,colNum) in columns" :key="`row-${rowNum}_col-${colNum}`">
-                {{ datum[column.name] }}
-            </component>
+            <template v-for="(column,colNum) in columns">
+
+              <td v-if="isTD(column)"
+              v-html="dataFilter( datum[column.name] , datum, column)"
+              v-bind="getProps('data',datum,column)"
+              v-on="getEvents('data',datum,column)"
+              :is="getDataComponent(column)"
+              :id="`cell-data-row-${rowNum}_col-${colNum}`"
+              :key="`row-${rowNum}_col-${colNum}`"
+              ref="cell-data"
+              >
+
+              </td>
+
+              <component v-else
+              v-bind="getProps('data',datum,column)"
+              v-on="getEvents('data',datum,column)"
+              :is="getDataComponent(column)"
+              :id="`cell-data-row-${rowNum}_col-${colNum}`"
+              :key="`row-${rowNum}_col-${colNum}`"
+              ref="cell-data"
+              >
+              </component>
+
+            </template>
 
           </component>
         </template>
 
         <template v-else>
-          <tr v-if="data.length==0">
+          <tr v-if="data.length==0 && noContentText">
             <td style="text-align:center" :colspan="(columns.length)">{{noContentText}}</td>
           </tr>
         </template>
@@ -52,6 +73,14 @@ export default {
     numberKey: {
       type: String,
       default: 'no'
+    },
+    config: {
+      type: Object,
+      required: false
+    },
+    noContentText: {
+      type: String,
+      default: ''
     }
   },
   computed: {
@@ -77,15 +106,92 @@ export default {
     }
   },
   methods: {
-    getHeaderComponent () {
-      return 'th'
+    getHeaderComponent (column) {
+      let config = column['header']
+      if (!config || !config.component) {
+        return 'th'
+      }
+      return config.component || 'th'
     },
     getRowComponent () {
       return 'tr'
     },
-    getDataComponent () {
-      return 'td'
+    getDataComponent (column) {
+      let config = column['data']
+      if (!config || !config.component) {
+        return 'td'
+      }
+      return config.component || 'td'
+    },
+    isTD (column) {
+      return this.getDataComponent(column) === 'td'
+    },
+    dataFilter (data, model, column) {
+      let filter = column.filter
+
+      if (!filter || typeof filter !== 'function') {
+        return data
+      }
+
+      return filter(data, model)
+    },
+    getEvents (type = 'data', model, column) {
+      let config = column.events || {}
+      let events = {}
+
+      for (let key in config) {
+        events[key] = () => {
+          config[key](model)
+        }
+      }
+
+      return events
+    },
+    getClassObject (classProp) {
+      if (!classProp) {
+        classProp = {}
+      }
+
+      if (typeof classProp !== 'object') {
+        classProp = classProp.split(' ')
+      }
+
+      if (Array.isArray(classProp)) {
+        return classProp.reduce((result, item, index, array) => {
+          result[item] = true
+          return result
+        }, {})
+      }
+      return classProp
+    },
+    getProps (type = 'data', model, column) {
+      let props = {}
+      let config = column[type]
+      if (config && config.props) {
+        props = config.props
+      }
+      props.model = model
+      let defaultClasses = this.getClassObject([
+        `${type}-cell-${column.name}`
+      ])
+      props.class = Object.assign(this.getClassObject(props.class), defaultClasses)
+      return props
+    },
+    initializeComponents () {
+      let config = this.config
+      let components = config ? config.components : null
+      if (!components) {
+        return
+      }
+
+      for (let key in components) {
+        let component = components[key]
+        this.$options.components[component.name] = component
+      }
     }
+  },
+  created (vm) {
+    this.initializeComponents()
   }
 }
 </script>
