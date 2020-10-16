8887b9fa6fc94eb01994a0a5727d5d12eb740b6a
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-10-16 17:39:13 +0800

    UPDATE

--- src/components/table/helper.js
+++ src/components/table/helper.js
@@ -1,13 +1,32 @@
+export const toClassObject = (classProp) => {
+  if (!classProp || (typeof classProp === 'object' && !Array.isArray(classProp))) {
+    return {}
+  }
+
+  if (typeof classProp === 'string') {
+    classProp = classProp.split(' ')
+  }
+
+  if (Array.isArray(classProp)) {
+    return classProp.reduce((result, item, index, array) => {
+      result[item] = true
+      return result
+    }, {})
+  }
+  return classProp
+}
+
+export const getProperty = (_config, key, defaultValue) => {
+  let config = typeof _config === 'object' ? _config : {}
+  if (config.hasOwnProperty(key)) {
+    return config[key] || defaultValue
+  }
+  return defaultValue
+}
+
 export const make = {
-  getConfig (_config, key) {
-    let config = typeof _config === 'object' ? _config : {}
-    if (config.hasOwnProperty(key)) {
-      return config[key]
-    }
-    return config
-  },
   events (_config, params) {
-    let config = this.getConfig(_config)
+    let config = getProperty(_config, 'events', {})
     return Object.keys(config).reduce((events, key) => {
       if (typeof config[key] === 'function') {
         events[key] = () => { config[key](params) }
@@ -15,38 +34,16 @@ export const make = {
       return events
     }, {})
   },
-  component (config, defaultComponent, params = {}) {
-    if (!config || !config.component) {
-      return defaultComponent
-    }
-    if (typeof config.component === 'function') {
-      return config.component(params)
-    }
-    return config.component || defaultComponent
+  component (_config, defaultComponent, params = {}) {
+    let component = getProperty(_config, 'component', defaultComponent)
+    return typeof component === 'function' ? component(params) : component
   },
   props (config, defaults = {}) {
-    let props = config.props || {}
+    let props = getProperty(config, 'props', {})
     props.class = this.mergeClasses(props.class, defaults.class)
     return Object.assign(defaults, props)
   },
   mergeClasses (target, defaults) {
-    let makeClassObject = (classProp) => {
-      if (!classProp || (typeof classProp === 'object' && !Array.isArray(classProp))) {
-        return {}
-      }
-
-      if (typeof classProp === 'string') {
-        classProp = classProp.split(' ')
-      }
-
-      if (Array.isArray(classProp)) {
-        return classProp.reduce((result, item, index, array) => {
-          result[item] = true
-          return result
-        }, {})
-      }
-      return classProp
-    }
-    return Object.assign(makeClassObject(defaults), makeClassObject(target))
+    return Object.assign(toClassObject(defaults), toClassObject(target))
   }
 }
